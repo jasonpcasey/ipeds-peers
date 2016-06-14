901105f54df1cec0d9d53a8640053ef353b68cf1
jasonpcasey <jazzerj@gmail.com>
2016-06-14 16:28:22 -0400

    data read and test graph creation

--- scripts/read-data.R
+++ scripts/read-data.R
@@ -11,8 +11,9 @@ library(RODBC)
 # Open a connection
 con <- odbcConnect("OSPIR-DEV")
 
-# Retrieve Institutional Data
-institutions <- sqlQuery(con, "SELECT [Unitid]
+# Retrieve institutional data as NODES (the objects in the igraph)
+# Contains unitid's and several institution attributes
+nodes <- sqlQuery(con, "SELECT [Unitid]
                          ,[InstitutionName]
                          ,[StateAbbreviation] AS [State]
                          ,[RegionLiteral]
@@ -35,14 +36,45 @@ institutions <- sqlQuery(con, "SELECT [Unitid]
                          [CarnegieBasic2010Code] > 0 AND
                          [IsClosed] = 0")
 
-# Retrieve Peer Lists
+# Retrieve peer lists (the EDGES or connections between NODES)
 # Contains the unitid of the selecting institution and the unitid's of its selections
-peer.lists <- sqlFetch(con, "extract.DFRPeerLists")
+# Because there can be no edges without corresponding nodes, I've used WHERE clauses to ensure
+#    that the same criteria are used for unitid and peer unitid as were used in the NODES query
+edges <- sqlQuery(con, "SELECT [UnitID] AS [Unitid]
+                         ,[PeerUnitID] AS [PeerUnitid]
+                         FROM extract.DFRPeerLists WHERE [UnitID] IN
+                         (SELECT [Unitid]
+                         FROM  [staged].[Institutions]
+                         WHERE [ControlCode] IN (1,2)  AND
+                         [LevelCode] = 1 AND 
+                         [CarnegieBasic2010Code] > 0 AND
+                         [IsClosed] = 0)
+                         AND [PeerUnitID] IN
+                         (SELECT [Unitid]
+                         FROM  [staged].[Institutions]
+                         WHERE [ControlCode] IN (1,2)  AND
+                         [LevelCode] = 1 AND 
+                         [CarnegieBasic2010Code] > 0 AND
+                         [IsClosed] = 0)")
 
 #close connection
 close(con)
 rm(con)
 
-# Join the files
+# create igraph object from nodes and edges
+g <- graph_from_data_frame(d=edges, vertices=nodes, directed=TRUE)
+
+# Set edge names to institution names rather than unitid's
+V(g)$name <- V(g)$InstitutionName
+
+#plot the network
+plot(g)
+
+# total <- merge(data frameA,data frameB,by=c("ID","Country"))
+
 # Make counts
 # Create aggregates
+# Raw Peer Score = Peer List Counts
+# Aspirant Estimation (Reciprocity) -- % Reciprocity in Peer List
+# Weighted Peer Score 1 -- Raw Score * Reciprocal Raw Score
+# Weighted Peer Score 2 -- Raw Score - Segment Mean
